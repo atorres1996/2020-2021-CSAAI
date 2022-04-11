e483847fb223453dc7134f3fada947913822449b
atorres1996 <a.torresh.2018@alumnos.urjc.es>
2022-04-11 21:38:55 +0200

    no consigo meter sonidos

--- P3/index.js
+++ P3/index.js
@@ -4,8 +4,9 @@ console.log("Ejecutando JS...");
 const canvas = document.getElementById("canvas");
 canvas.width = 800;
 canvas.height = 500;
-canvas.position = 15;
+
 const ctx = canvas.getContext("2d");
+
 
 //variables y constantes de la pelota
 var radio = 9;
