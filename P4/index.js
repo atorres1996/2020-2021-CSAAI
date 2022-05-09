31943fbf9b6e9ffdbea5e7a5f5e5ca1d389a3df1
atorres1996 <a.torresh.2018@alumnos.urjc.es>
2022-05-09 12:50:51 +0200

    preparando

--- P4/index.js
+++ P4/index.js
@@ -1,26 +1,129 @@
 console.log("Ejecutando JS....")
 
-//-- Obtener elementos del DOM
+
 const canvas = document.getElementById('canvas');
-const img = document.getElementById('imagesrc')
+const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
 
-//-- Función de retrollamada de imagen cargada
-//-- La imagen no se carga al instante, sino que
-//-- lleva un tiempo. Sólo podemos acceder a ella una vez
-//-- que esté totalmente cargada
+const gris = document.getElementById('gris');
+const decolor = document.getElementById('decolor');
+const reset = document.getElementById('reset');
+
+
+const deslizadorR = document.getElementById('deslizadorR');
+const deslizadorG = document.getElementById('deslizadorG');
+const deslizadorB = document.getElementById('deslizadorB');
+
+
+const range_valueR = document.getElementById('range_valueR');
+const range_valueG = document.getElementById('range_valueG');
+const range_valueB = document.getElementById('range_valueB');
+
+
+const ESTADO = {
+    GRIS: 1,
+    COLOR: 0,
+}
+
+let estado = ESTADO.COLOR;
+
+
 img.onload = function () {
 
-  console.log("Imagen cargada");
-
-  //-- Se establece como tamaño del canvas el mismo
-  //-- que el de la imagen original
   canvas.width = img.width;
   canvas.height = img.height;
-
-  //-- Situar la imagen original en el canvas
-  //-- No se han hecho manipulaciones todavia
+  
   ctx.drawImage(img, 0,0);
+
+  console.log("Imagen lista...");
 };
+
+
+function color() {
+    ctx.drawImage(img, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data
+
+
+    range_valueR.innerHTML = deslizadorR.value;
+    umbralR = deslizadorR.value
+
+    range_valueG.innerHTML = deslizadorG.value;
+    umbralG = deslizadorG.value
+
+    range_valueB.innerHTML = deslizadorB.value;
+    umbralB = deslizadorB.value
+
+    for (let i = 0; i < data.length; i+=4) {
+        if (data[i] > umbralR){
+            data[i] = umbralR;
+        }
+        if (data[i+1] > umbralG){
+            data[i+1] = umbralG;
+        }
+        if (data[i+2] > umbralB){
+            data[i+2] = umbralB;
+        }
+    }
+  ctx.putImageData(imgData, 0, 0);
+}
+
+function todogris() {
+    ctx.drawImage(img, 0,0);
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data = imgData.data
+    ctx.drawImage(img, 0,0);
+
+    for (let i = 0; i < data.length; i+=4) {
+        (3*data[i] + 4*data[i+1]+ data[i+2])/8
+        data[i] = data[i+1] = data[i+2] 
+      }
+
+  ctx.putImageData(imgData, 0, 0);
+
+  estado = ESTADO.GRIS;
+
+}
+
+
+deslizadorR.oninput = () => {
+    if (estado == ESTADO.COLOR){
+        color();
+    }
+}
+
+deslizadorG.oninput = () => {
+    if (estado == ESTADO.COLOR){
+        color();
+    }
+}
+
+deslizadorB.oninput = () => {
+    if (estado == ESTADO.COLOR){
+        color();
+    }
+}
+
+gris.onclick = () => {
+    todogris();
+}
+
+decolor.onclick = () => {
+    estado = ESTADO.COLOR;
+    color ();
+}
+
+reset.onclick = () => {
+    deslizadorR.value = 255;
+    deslizadorG.value = 255;
+    deslizadorB.value = 255;
+
+    range_valueR.innerHTML = deslizadorR.value;
+    range_valueG.innerHTML = deslizadorG.value;
+    range_valueB.innerHTML = deslizadorB.value;
+
+    ctx.drawImage(img, 0,0);
+    estado = ESTADO.COLOR;
+}
 
 console.log("Fin...");
